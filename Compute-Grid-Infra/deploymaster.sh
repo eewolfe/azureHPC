b34123aebe3fc2a2f0cb637d5576ed94e5afd249
Steven Roach <stevenwroach@hotmail.com>
2017-07-16 02:25:24 -0700

    Update deploymaster.sh

--- Compute-Grid-Infra/deploymaster.sh
+++ Compute-Grid-Infra/deploymaster.sh
@@ -1,2 +1,2 @@
-az group create ready southcentralus
-az group deployment create ready labdeployment  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Ready/Compute-Grid-Infra/deploy-master.json  --parameters @master.param.json
+az group create -n ready -l southcentralus
+az group deployment create -g ready -n labdeployment  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Ready/Compute-Grid-Infra/deploy-master.json  --parameters @master.param.json
