02061aab771e2bb4eda66b3cd71de4f3b42a813b
Steven Roach <stevenwroach@hotmail.com>
2017-07-19 23:41:48 -0700

    Delete deploymaster.sh

--- Compute-Grid-Infra/deploymaster.sh
+++
@@ -1,2 +0,0 @@
-az group create -n ready -l southcentralus
-az group deployment create -g ready -n labdeployment  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Ready/Compute-Grid-Infra/deploy-master.json  --parameters @master.param.json
