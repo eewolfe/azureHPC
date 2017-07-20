c4b571baa604c67875bf5278cd65f47e4f534214
Steven Roach <stevenwroach@hotmail.com>
2017-07-20 00:54:58 -0700

    Update deploycluster.sh

--- Compute-Grid-Infra/deploycluster.sh
+++ Compute-Grid-Infra/deploycluster.sh
@@ -1,3 +1,3 @@
 az group create -n ready1 -l southcentralus
-az group deployment create -g ready1 -n deploymaster  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Ready/Compute-Grid-Infra/deploy-master.json  --parameters @param.json
-az group deployment create -g ready1 -n deploynodes  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Ready/Compute-Grid-Infra/deploy-nodes.json  --parameters @param.json
+az group deployment create -g ready1 -n deploymaster  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Ready/Compute-Grid-Infra/deploy-master.json  --parameters @master.param.json
+az group deployment create -g ready1 -n deploynodes  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Ready/Compute-Grid-Infra/deploy-nodes.json  --parameters @nodes.param.json
