ae55494091b8ef28387ad6a8c7fa502fba7d4ce9
Steven Roach <stevenwroach@hotmail.com>
2017-11-08 06:11:09 -0700

    Update deploycluster.sh

--- Compute-Grid-Infra/deploycluster.sh
+++ Compute-Grid-Infra/deploycluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RGNAME=`az group list --output table | grep '\-02' | cut -d' ' -f1`
+RGNAME=`az group list --output table | grep '\-03' | cut -d' ' -f1`
 az group deployment create -g $RGNAME -n deploymaster  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Lab/Compute-Grid-Infra/deploy-master.json  --parameters @master.param.json
 az group deployment create -g $RGNAME -n deploynodes  --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/Lab/Compute-Grid-Infra/deploy-nodes.json  --parameters @nodes.param.json
