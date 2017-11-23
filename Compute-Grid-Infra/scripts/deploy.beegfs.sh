5b31826f4478a28bdacd7665300da1bb2faaaedb
root <root@deployVM.s12ayegxogne1o0z3vd1pzlvvc.jx.internal.cloudapp.net>
2017-11-23 06:06:55 +0000

    Updated for eewolfe github URL.

--- Compute-Grid-Infra/scripts/deploy.beegfs.sh
+++ Compute-Grid-Infra/scripts/deploy.beegfs.sh
@@ -6,6 +6,7 @@ cat beegfs.param.json.tmpl >> beegfs.param.json
 sed -i "s/spix/$1/g" beegfs.param.json
 cat beegfs.param.json
 
-az group deployment create -g $1 --template-uri https://raw.githubusercontent.com/grandparoach/azure-hpc/custom2/Compute-Grid-Infra/BeeGFS/deploy-beegfs-vmss.json --parameters @beegfs.param.json
+az account set -s "Commercial-Chevron"
+az group deployment create -g $1 --template-uri https://raw.githubusercontent.com/eewolfe/azureHPC/custom2/Compute-Grid-Infra/BeeGFS/deploy-beegfs-vmss.json --parameters @beegfs.param.json
 
 rm beegfs.param.json
