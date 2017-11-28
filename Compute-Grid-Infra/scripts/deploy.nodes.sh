ec9a5f1a2bdecec0f79336c7e35e68fd33c8e803
Gene Wolfe <gene.wolfe@halliburton.com>
2017-11-28 22:54:57 +0000

    Updated and cleaned up.

--- Compute-Grid-Infra/scripts/deploy.nodes.sh
+++ Compute-Grid-Infra/scripts/deploy.nodes.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
+
 set -x 
 
-cat nodes.param.json.tmpl >> nodes.param.json
-
-sed -i "s/spix/$1/g" nodes.param.json
-cat nodes.param.json
-
-az account set -s "Commercial-Chevron"
+az account set -s ""
 az group deployment create -g $1 --template-uri https://raw.githubusercontent.com/eewolfe/azureHPC/custom2/Compute-Grid-Infra/deploy-nodes.json --parameters @nodes.param.json
-
-rm nodes.param.json
-
