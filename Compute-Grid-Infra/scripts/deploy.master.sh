b968e5f616392d7d5b11dcd53d24a8bbdbd7f8c9
root <root@deployVM.s12ayegxogne1o0z3vd1pzlvvc.jx.internal.cloudapp.net>
2017-11-28 22:47:34 +0000

    Updated and clean up.

--- Compute-Grid-Infra/scripts/deploy.master.sh
+++ Compute-Grid-Infra/scripts/deploy.master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 set -x 
 
-az account set -s "Commercial-Chevron"
+az account set -s ""
 az group create -n $1 -l southcentralus
 az group deployment create -g $1 --template-uri https://raw.githubusercontent.com/eewolfe/azureHPC/custom2/Compute-Grid-Infra/deploy-master.json --parameters @master.param.json
