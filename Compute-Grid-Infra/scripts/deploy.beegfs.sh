3d1a5c520476442f188d4fd2f5def43e356c635b
Gene Wolfe <gene.wolfe@halliburton.com>
2017-11-28 22:52:57 +0000

    Updated and cleaned up.

--- Compute-Grid-Infra/scripts/deploy.beegfs.sh
+++ Compute-Grid-Infra/scripts/deploy.beegfs.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
+
 set -x 
 
-cat beegfs.param.json.tmpl >> beegfs.param.json
-
-sed -i "s/spix/$1/g" beegfs.param.json
-cat beegfs.param.json
-
-az account set -s "Commercial-Chevron"
+az account set -s ""
 az group deployment create -g $1 --template-uri https://raw.githubusercontent.com/eewolfe/azureHPC/custom2/Compute-Grid-Infra/BeeGFS/deploy-beegfs-vmss.json --parameters @beegfs.param.json
-
-rm beegfs.param.json
