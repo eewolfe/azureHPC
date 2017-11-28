6bc3cfff9d85fc75a714583a471147f2b9e78117
Gene Wolfe <gene.wolfe@halliburton.com>
2017-11-28 22:53:52 +0000

    Updated and cleaned up.

--- Compute-Grid-Infra/scripts/deploy.seisspace.sh
+++ Compute-Grid-Infra/scripts/deploy.seisspace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 set -x 
-az account set -s "Commercial-Chevron"
+
+az account set -s ""
 az group deployment create -g $1 --template-uri https://raw.githubusercontent.com/eewolfe/azureHPC/custom2/Compute-Grid-Infra/custom/custom-script.json --parameters @seisspace.param.json
 
