6a9033d68aac4796022ddd2168c456c1ac473db8
Mike Zawacki <mzawacki@microsoft.com>
2017-12-04 08:59:38 -0800

    better handle the scaling of BeeGFS without losing data

--- Compute-Grid-Infra/scripts/shutdown.sh
+++ Compute-Grid-Infra/scripts/shutdown.sh
@@ -3,7 +3,10 @@
 set -x 
 
 rg=$1
-az vmss scale -g $rg -n BeeGFS --new-capacity 0 --no-wait
+# DONT USE vmss scale --new-capacity 0 
+# THIS WILL CAUSE THE VMs to lose any persistent data when the scale set is restarted
+#az vmss scale -g $rg -n BeeGFS --new-capacity 0 --no-wait
+az vmss deallocate -n BeeGFS --instance-ids * --no-wait
 az vmss scale -g $rg -n spi00 --new-capacity 0 --no-wait
 az vm stop -g $rg -n spimaster --no-wait
 
--- Compute-Grid-Infra/scripts/startup.sh
+++ Compute-Grid-Infra/scripts/startup.sh
@@ -3,7 +3,7 @@
 set -x 
 
 rg=$1
-az vmss scale -g $rg -n BeeGFS --new-capacity 2 --no-wait
+az vmss scale -g $rg -n BeeGFS --instance-ids * --no-wait
 az vmss scale -g $rg -n spi00 --new-capacity 2 --no-wait
 az vm start -g $rg -n spimaster --no-wait
 
