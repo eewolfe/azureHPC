af3b94c3bc014b8d6b950cbe38b72eb1764fa256
root <root@deployVM.gdj1h3qh4qruhadbda4ctcawlh.jx.internal.cloudapp.net>
2017-12-13 00:49:26 +0000

    Corrected order of execution for shutting down or deallocating.

--- Compute-Grid-Infra/scripts/shutdown.sh
+++ Compute-Grid-Infra/scripts/shutdown.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+########################################
+# $1 is the Resource Group
+# $2 is the prefix for the VM Scale Set
+########################################
+
 set -x 
 
 rg=$1
+
 # DONT USE vmss scale --new-capacity 0 
 # THIS WILL CAUSE THE VMs to lose any persistent data when the scale set is restarted
-#az vmss scale -g $rg -n BeeGFS --new-capacity 0 --no-wait
-az vmss deallocate -n BeeGFS --instance-ids * --no-wait
-az vmss scale -g $rg -n spi00 --new-capacity 0 --no-wait
-az vm stop -g $rg -n spimaster --no-wait
+
+az vmss deallocate -g $rg -n BeeGFS --instance-ids "*" --no-wait
+az vmss scale -g $rg -n $200 --new-capacity 0
+az vm deallocate -g $rg -n $2master --no-wait
 
