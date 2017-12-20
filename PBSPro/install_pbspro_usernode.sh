9fa7a3375a3687c51d31451d6b4b99b0d822df57
eewolfe <30878366+eewolfe@users.noreply.github.com>
2017-12-20 11:22:53 -0700

    Update install_pbspro_usernode.sh
    Removed master and compute node install and configuration portions. Only installs PBSpro on user nodes.

--- PBSPro/install_pbspro_usernode.sh
+++ PBSPro/install_pbspro_usernode.sh
@@ -16,72 +16,15 @@ fi
 MASTER_HOSTNAME=$1
 PBS_MANAGER=hpcuser
 
-# Returns 0 if this node is the master node.
-#
-is_master()
-{
-    hostname | grep "$MASTER_HOSTNAME"
-    return $?
-}
-
-enable_kernel_update()
-{
-	# enable kernel update
-	sed -i.bak -e '28d' /etc/yum.conf 
-	sed -i '28i#exclude=kernel*' /etc/yum.conf 
-
-}
-# Installs all required packages.
-#
-install_pkgs()
-{
-    yum -y install epel-release
-    yum -y install zlib zlib-devel bzip2 bzip2-devel bzip2-libs openssl openssl-devel openssl-libs gcc gcc-c++ nfs-utils rpcbind mdadm wget python-pip
-}
-
 # Downloads and installs PBS Pro OSS on the node.
-# Starts the PBS Pro control daemon on the master node and
-# the mom agent on worker nodes.
 #
 install_pbspro()
 {    
     wget -O /mnt/CentOS_7.zip  http://wpc.23a7.iotacdn.net/8023A7/origin2/rl/PBS-Open/CentOS_7.zip
     unzip /mnt/CentOS_7.zip -d /mnt
        
-    if is_master; then
-    	enable_kernel_update
-	install_pkgs
-	
-    	yum install -y gcc make rpm-build libtool hwloc-devel libX11-devel libXt-devel libedit-devel libical-devel ncurses-devel perl postgresql-devel python-devel tcl-devel tk-devel swig expat-devel openssl-devel libXext libXft autoconf automake expat libedit postgresql-server python sendmail tcl tk libical perl-Env perl-Switch
-    	# Required on 7.2 as the libical lib changed
-    	ln -s /usr/lib64/libical.so.1 /usr/lib64/libical.so.0
-
-	rpm -ivh --nodeps /mnt/CentOS_7/pbspro-server-14.1.0-13.1.x86_64.rpm
-
-        cat > /etc/pbs.conf << EOF
-PBS_SERVER=$MASTER_HOSTNAME
-PBS_START_SERVER=1
-PBS_START_SCHED=1
-PBS_START_COMM=1
-PBS_START_MOM=0
-PBS_EXEC=/opt/pbs
-PBS_HOME=/var/spool/pbs
-PBS_CORE_LIMIT=unlimited
-PBS_SCP=/bin/scp
-EOF
-    
-        /etc/init.d/pbs start
-        
-        # Enable job history
-        /opt/pbs/bin/qmgr -c "s s job_history_enable = true"
-        /opt/pbs/bin/qmgr -c "s s job_history_duration = 336:0:0"
-
-		# add hpcuser as manager
-        /opt/pbs/bin/qmgr -c "s s managers = $PBS_MANAGER@*"
-
-    else
-    	yum install -y hwloc-devel expat-devel tcl-devel expat
-	    rpm -ivh --nodeps /mnt/CentOS_7/pbspro-execution-14.1.0-13.1.x86_64.rpm
+    yum install -y hwloc-devel expat-devel tcl-devel expat
+    rpm -ivh --nodeps /mnt/CentOS_7/pbspro-execution-14.1.0-13.1.x86_64.rpm
 
         cat > /etc/pbs.conf << EOF
 PBS_SERVER=$MASTER_HOSTNAME
@@ -94,15 +37,8 @@ PBS_HOME=/var/spool/pbs
 PBS_CORE_LIMIT=unlimited
 PBS_SCP=/bin/scp
 EOF
-
-		echo '$clienthost '$MASTER_HOSTNAME > /var/spool/pbs/mom_priv/config
-        /etc/init.d/pbs start
-
-    fi
     echo 'export PATH=/opt/pbs/bin:$PATH' >> /etc/profile.d/pbs.sh
     echo 'export PATH=/opt/pbs/sbin:$PATH' >> /etc/profile.d/pbs.sh
-
-    cd ..
 }
 
 SETUP_MARKER=/var/local/install_pbspro.marker
