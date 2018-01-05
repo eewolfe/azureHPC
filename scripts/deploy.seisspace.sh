0f1551db0527165f0f313896d3097b944f260ff7
Gene Wolfe <gene.wolfe@halliburton.com>
2018-01-05 00:53:49 +0000

    Updated to call seisspace instead of custom.

--- scripts/deploy.seisspace.sh
+++ scripts/deploy.seisspace.sh
@@ -9,6 +9,6 @@ if [ -z $SCRIPT_URL ]; then
     SCRIPT_URL='https://raw.githubusercontent.com/eewolfe/azureHPC/master'
 fi
 
-templateuri=$SCRIPT_URL'/custom/custom-script.json'$SCRIPT_SASKEY
+templateuri=$SCRIPT_URL'/custom/seisspace-script.json'$SCRIPT_SASKEY
 
 az group deployment create -g $1 --template-uri "$templateuri" --parameters @seisspace.param.json
