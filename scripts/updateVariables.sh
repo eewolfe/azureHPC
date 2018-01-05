6075a227f243c8ed277de21a98ace87d85d6a746
Gene Wolfe <gene.wolfe@halliburton.com>
2018-01-05 16:16:37 +0000

    Removed invalid keys and account names.

--- scripts/updateVariables.sh
+++ scripts/updateVariables.sh
@@ -6,23 +6,26 @@ set -x
 
 # Master Node
 
-export vmPrefix='cvx'
+export vmPrefix=''
 export newOrExisting='existing'
-export resourceGroup='cvx-shared-rg'
-export virtualNetworkName='cvx-scus-vnet01'
-export sshKeyData='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5LKolefwtgFfbrPCNfBEnVuJ8XSd3ArymEzcfJC/wd/4bwySJIHFVPFErd001WffGP73Yz/90NUXYeVVIFLcdGhPusr3RzNVKMAdxX0LDdMwk/XO/vu09jm9vSgQCO6xrzYF29bjVXKTlIHBV3LlZxly+6s0dFKyqNPGyg8PsDvYVs5SAGrJSWmEPNrkXgNF9p6ONqhS1LgiT4OawZaaz2Bdz/rLNh8lDz5U/B+ZsKKvtk0eRTcEIjkh7jnKDS+KrYBSEA/vZTJdjR3FpoyTDG5ti2AL7USAwqHweLCVXFPJ01DjoWzlCtSnavw3/LvZxJ3tTpYBeB17Iih7d3rmX'
+export resourceGroup=''
+export virtualNetworkName=''
+export sshKeyData=''
 
 # Beegfs
 
 # SeisSpace
-export storageAccount='seisspacefg'
-export storageKey='6OWbnGTVwHJXXHm8U8Da4Hc3BLCIFjUWGbmZEfpYLAYvxCY51U5CRC3wSWQd2Zsxf9qDK4YGuuDNjAK+CMSSQQ=='
+export storageAccount=''
+export storageKey=''
 
 # Nodes
 export vmSSprefix='node'
 
+# User nodes
+export vmNamePrefix='nav'
 
-# Update master node deployment variables
+
+# param.json update functions
 
 update_master () {
 
@@ -57,12 +60,23 @@ update_nodes () {
   sed -i "s|VNN|$virtualNetworkName|"  ./nodes.param.json
   sed -i "s|ssh-rsa|$sshKeyData|"      ./nodes.param.json
   sed -i "s|VMP|$vmPrefix|"            ./nodes.param.json
-  sed -i "s|VMPC|#vmSSprefix|"         ./nodes.param.json
+  sed -i "s|VMPC|$vmSSPrefix|"         ./nodes.param.json
+
 }
 
-#update_master
-#update_beegfs
+update_usernode () {
+
+  sed -i "s|RGP|$resourceGroup|"       ./usernode.param.json
+  sed -i "s|VNN|$virtualNetworkName|"  ./usernode.param.json
+  sed -i "s|ssh-rsa|$sshKeyData|"      ./usernode.param.json
+  sed -i "s|VMP|$vmPrefix|"            ./usernode.param.json
+  sed -i "s|VMPN|$vmNamePrefix|"       ./usernode.param.json
+
+}
+
+update_master
+update_beegfs
 update_seisspace
-#update_nodes
+update_nodes
 
 exit 0
