38276795273a43baacf54873f1cfb7bc5157f87d
eewolfe <30878366+eewolfe@users.noreply.github.com>
2018-01-05 18:36:42 -0700

    Update cn-setup.sh
    Fixed some issues with sudo and group ID

--- ComputeNodes/scripts/cn-setup.sh
+++ ComputeNodes/scripts/cn-setup.sh
@@ -171,13 +171,12 @@ setup_user()
 	echo "$MASTER_NAME:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
 	mount -a
 	mount
+	
+    groupadd -g $HPC_GID $HPC_GROUP
 
     # Don't require password for HPC user sudo
     echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
     
-    # Disable tty requirement for sudo
-    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
-
     useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
 
     chown $HPC_USER:$HPC_GROUP $SHARE_SCRATCH	
@@ -187,6 +186,9 @@ setup_user()
 ######################################################################
 
 #install_applications
+
+# Disable tty requirement for sudo
+    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
 
 SETUP_MARKER=/var/local/cn-setup.marker
 if [ -e "$SETUP_MARKER" ]; then
