28a5ff71a94ab55f718545b150c48d5c85f3de81
eewolfe <30878366+eewolfe@users.noreply.github.com>
2019-02-23 08:22:17 -0700

    Added in ability to substitute in VMP variable for masterName

--- scripts/deploy.all.sh
+++ scripts/deploy.all.sh
@@ -5,13 +5,13 @@ set -x
 #INPUT
 rg=$1
 vmssName=node00
-masterName=spimaster
+masterName=VMPmaster
 
 ./deploy.master.sh $rg
 
-az group deployment show -n deploy-master -g $rg --query "properties.outputs"
-custStorage=$(az group deployment show -n deploy-master -g $rg --query "properties.outputs.customerStorage.value" --out tsv)
-hostname=$(az group deployment show -n deploy-master -g $rg --query "properties.outputs.masterFQDN.value" --out tsv)
+az group deployment show -n $masterName -g $rg --query "properties.outputs"
+custStorage=$(az group deployment show -n $masterName -g $rg --query "properties.outputs.customerStorage.value" --out tsv)
+hostname=$(az group deployment show -n $masterName -g $rg --query "properties.outputs.masterFQDN.value" --out tsv)
 # Get public IP
 # Get customer storage account
 
