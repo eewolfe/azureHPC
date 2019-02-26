5b6e215a57da5ca50010c6daa5e52890c09f6f00
eewolfe <30878366+eewolfe@users.noreply.github.com>
2019-02-25 17:16:32 -0700

    Update install_pbspro.sh

--- PBSPro/install_pbspro.sh
+++ PBSPro/install_pbspro.sh
@@ -45,8 +45,8 @@ install_pkgs()
 #
 install_pbspro()
 {    
-    #wget -O /mnt/CentOS_7.zip  http://wpc.23a7.iotacdn.net/8023A7/origin2/rl/PBS-Open/CentOS_7.zip
-    wget -O /mnt/CentOS_7.zip  https://solliancehpcstrg.blob.core.windows.net/pbspro/CentOS_7.zip
+    wget -O /mnt/CentOS_7.zip  http://wpc.23a7.iotacdn.net/8023A7/origin2/rl/PBS-Open/CentOS_7.zip
+    #wget -O /mnt/CentOS_7.zip  https://solliancehpcstrg.blob.core.windows.net/pbspro/CentOS_7.zip
     unzip /mnt/CentOS_7.zip -d /mnt
        
     if is_master; then
