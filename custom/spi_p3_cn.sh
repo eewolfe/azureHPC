bcb47a64db2c9f68cbe8a7b3091c9addc38d75c5
eewolfe <30878366+eewolfe@users.noreply.github.com>
2019-04-03 10:07:34 -0600

    Added security  agent install

--- custom/spi_p3_cn.sh
+++ custom/spi_p3_cn.sh
@@ -25,11 +25,61 @@ while [ $user_count -le $1 ]
 }
 
 #####################################
+# Function to setup for blob transfer
+#####################################
+
+blob_setup () {
+
+export azstgacct=$1
+export BLOBXFER_STORAGEACCOUNTKEY=$2
+export BLOBDIR=$3
+
+#  Install blobxfer
+echo "Provision blobxfer"
+yum -y install epel-release gcc libffi-devel openssl-devel python-devel
+
+#  Download and install pip for RHEL 7
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+
+wget https://pypi.python.org/packages/source/s/setuptools/setuptools-7.0.tar.gz --no-check-certificate
+tar xzf setuptools-7.0.tar.gz
+cd setuptools-7.0
+python setup.py install
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+cd ..
+ pip install --upgrade --force-reinstall pip==9.0.3
+ pip install blobxfer --disable-pip-version-check
+ pip install --upgrade pip
+ pip install blobxfer --upgrade
+ 
+}
+
+######################################
+# Install cloud security files
+######################################
+
+function install_security () {
+
+echo "Downloading and installing CarbonBlack . . . "
+blobxfer download --storage-account $azstgacct --storage-account-key $BLOBXFER_STORAGEACCOUNTKEY --local-path . --remote-path "$BLOBDIR/CarbonBlack.sh" 
+  chmod 755 CarbonBlack.sh
+  ./CarbonBlack.sh
+
+echo "Downloading and installing QualysCloud . . . "
+blobxfer download --storage-account $azstgacct --storage-account-key $BLOBXFER_STORAGEACCOUNTKEY --local-path . --remote-path "$BLOBDIR/QualysCloud.sh"
+
+  chmod 755 QualysCloud.sh
+  ./QualysCloud.sh
+
+}
+
+#####################################
 # Function to setup system parameters
 #####################################
 
-setup_system()
-{
+setup_system() {
 
 # Disable transparent huge pages and open permissions on /mnt/resource
 echo "Disabling transparent huge page compaction."
@@ -89,5 +139,7 @@ add_users $numusers
   
 # Setup system parameters
 setup_system
+blob_setup $1 $2 $3
+install_security
 
 echo "Finished"
